cfb43a36694481cb21a63512dc9c777eb7a31308
Rakib Hasan <92901083+rakibhasan3948@users.noreply.github.com>
2023-10-23 14:46:47 +0600

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,19 +1,30 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'LoginScreen.dart';
-void main() {
+import 'login.dart';
+
+String email = 'rakib15-3948@diu.edu.bd', password = '1234';
+String mail ='',pass='';
+String wrong='';
+
+void main()
+{
   return runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
 
-      home: Login(),
+      home: Log(),
+
 
     );
   }
