a3338911bc1d1b59413b0f42268c641b9e495840
Rakib Hasan <92901083+rakibhasan3948@users.noreply.github.com>
2023-10-26 00:37:54 +0600

    Add files via upload

--- TicTacToeGames/game.dart
+++ TicTacToeGames/game.dart
@@ -1,288 +1,194 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:tic_tac_toe/main.dart';
 
-class game extends StatefulWidget {
-  const game({super.key});
-
+class GamePage extends StatefulWidget {
   @override
-  State<game> createState() => _gameState();
+  State<GamePage> createState() => _GamePageState();
 }
 
-class _gameState extends State<game> {
+class _GamePageState extends State<GamePage> {
+  static const String PLAYER_X = "X";
+  static const String PLAYER_Y = "O";
+
+  late String currentPlayer;
+  late bool gameEnd;
+  late List<String> occupied;
+
+  @override
+  void initState() {
+    initializeGame();
+    super.initState();
+  }
+
+  void initializeGame() {
+    currentPlayer = PLAYER_X;
+    gameEnd = false;
+    occupied = ["", "", "", "", "", "", "", "", ""]; //9 empty places
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Center(child: Text("TicTacToe",style: TextStyle(fontSize: 75,fontWeight: FontWeight.bold))),backgroundColor: Colors.blueGrey),
       backgroundColor: CupertinoColors.systemGrey4,
-
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-
-              Expanded(child: SizedBox()),
-
-             Row(
-               children: [
-
-
-               ],
-
-             ),
-
-
-              Row(
-                children: [
-                  Expanded(
-                    child: SizedBox(
-                      height: 100,
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStatePropertyAll<Color>(Colors.blueGrey)
-                          ),
-                          onPressed: (){
-                            count = count +1;
-                            setState(() {
-                              if(count % 2 == 1){
-                                symbol1 = 'X';
-                              }
-                              else if(count % 2 == 0){
-                                symbol1 = 'O';
-                              }
-                            });
-                          },
-                          child: Text("$symbol1",style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold))),
-                    ),
-                  ),
-                  SizedBox(width: 5),
-                  Expanded(
-                    child: SizedBox(
-                      height: 100,
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStatePropertyAll<Color>(Colors.blueGrey)
-                          ),
-                          onPressed: (){
-                            count = count +1;
-                            setState(() {
-                              if(count % 2 == 1){
-                                symbol2 = 'X';
-                              }
-                              else if(count % 2 == 0){
-                                symbol2 = 'O';
-                              }
-                            });
-                          },
-                          child: Text("$symbol2",style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold))),
-                    ),
-                  ),
-                  SizedBox(width: 5),
-                  Expanded(
-                    child: SizedBox(
-                      height: 100,
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStatePropertyAll<Color>(Colors.blueGrey)
-                          ),
-                          onPressed: (){
-                            count = count +1;
-                            setState(() {
-                              if(count % 2 == 1){
-                                symbol3 = 'X';
-                              }
-                              else if(count % 2 == 0){
-                                symbol3 = 'O';
-                              }
-                            });
-                          },
-                          child: Text("$symbol3",style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold))),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 5),
-              Row(
-                children: [
-                  Expanded(
-                    child: SizedBox(
-                      height: 100,
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStatePropertyAll<Color>(Colors.blueGrey)
-                          ),
-                          onPressed: (){
-                            count = count +1;
-                            setState(() {
-                              if(count % 2 == 1){
-                                symbol4 = 'X';
-                              }
-                              else if(count % 2 == 0){
-                                symbol4 = 'O';
-                              }
-                            });
-                          },
-                          child: Text("$symbol4",style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold))),
-                    ),
-                  ),
-                  SizedBox(width: 5),
-                  Expanded(
-                    child: SizedBox(
-                      height: 100,
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStatePropertyAll<Color>(Colors.blueGrey)
-                          ),
-                          onPressed: (){
-                            count = count +1;
-                            setState(() {
-                              if(count % 2 == 1){
-                                symbol5 = 'X';
-                              }
-                              else if(count % 2 == 0){
-                                symbol5 = 'O';
-                              }
-                            });
-                          },
-                          child: Text("$symbol5",style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold))),
-                    ),
-                  ),
-                  SizedBox(width: 5),
-                  Expanded(
-                    child: SizedBox(
-                      height: 100,
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStatePropertyAll<Color>(Colors.blueGrey)
-                          ),
-                          onPressed: (){
-                            count = count +1;
-                            setState(() {
-                              if(count % 2 == 1){
-                                symbol6 = 'X';
-                              }
-                              else if(count % 2 == 0){
-                                symbol6 = 'O';
-                              }
-                            });
-                          },
-                          child: Text("$symbol6",style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold))),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 5),
-              Row(
-                children: [
-                  Expanded(
-                    child: SizedBox(
-                      height: 100,
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStatePropertyAll<Color>(Colors.blueGrey)
-                          ),
-                          onPressed: (){
-                            count = count +1;
-                            setState(() {
-                              if(count % 2 == 1){
-                                symbol7 = 'X';
-                              }
-                              else if(count % 2 == 0){
-                                symbol7 = 'O';
-                              }
-                            });
-                          },
-                          child: Text("$symbol7",style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold))),
-                    ),
-                  ),
-                  SizedBox(width: 5),
-                  Expanded(
-                    child: SizedBox(
-                      height: 100,
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStatePropertyAll<Color>(Colors.blueGrey)
-                          ),
-                          onPressed: (){
-                            count = count +1;
-                            setState(() {
-                              if(count % 2 == 1){
-                                symbol8 = 'X';
-                              }
-                              else if(count % 2 == 0){
-                                symbol8 = 'O';
-                              }
-                            });
-                          },
-                          child: Text("$symbol8",style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold))),
-                    ),
-                  ),
-                  SizedBox(width: 5),
-                  Expanded(
-                    child: SizedBox(
-                      height: 100,
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: MaterialStatePropertyAll<Color>(Colors.blueGrey)
-                          ),
-                          onPressed: (){
-                            count = count +1;
-                            setState(() {
-                              if(count % 2 == 1){
-                                symbol9 = 'X';
-                              }
-                              else if(count % 2 == 0){
-                                symbol9 = 'O';
-                              }
-                            });
-                          },
-                          child: Text("$symbol9",style: TextStyle(fontSize: 50,fontWeight:FontWeight.bold))),
-                    ),
-                  ),
-                ],
-              ),
-
-              SizedBox(height: 20),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  //SizedBox(width: 160),
-                  Expanded(
-                    child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStatePropertyAll<Color>(CupertinoColors.systemRed)
-                        ),
-                        onPressed:(){
-                          setState(() {
-                            count = 0;
-                            symbol1 = '';
-                            symbol2 = '';
-                            symbol3 = '';
-                            symbol4 = '';
-                            symbol5 = '';
-                            symbol6 = '';
-                            symbol7 = '';
-                            symbol8 = '';
-                            symbol9 = '';
-                          });
-                        },
-                        child: Text("Restart",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40),)
-                    ),
-                  ),
-
-                ],
-
-              ),
-
-              Expanded(child: SizedBox()),
-
-            ],
-
+      body: Center(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _gameContainer(),
+                _restartButton(),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
 
+
+  Widget _gameContainer() {
+    return Container(
+      height: MediaQuery.of(context).size.height / 2,
+      width: MediaQuery.of(context).size.height / 2,
+      child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+          itemCount: 9,
+          itemBuilder: (context, int index) {
+            return _box(index);
+          }),
+    );
+  }
+
+  Widget _box(int index) {
+    return InkWell(
+      onTap: () {
+        //on click of box
+        if (gameEnd || occupied[index].isNotEmpty) {
+          //Return if game already ended or box already clicked
+          return;
+        }
+
+        setState(() {
+          occupied[index] = currentPlayer;
+          changeTurn();
+          checkForWinner();
+          checkForDraw();
+        });
+      },
+      child: Container(
+        color: occupied[index].isEmpty
+            ? Colors.blueGrey
+            : occupied[index] == PLAYER_X
+            ? Colors.blueGrey
+            : Colors.blueGrey,
+        margin: const EdgeInsets.all(8),
+        child: Center(
+          child: Text(
+            occupied[index],
+            style: const TextStyle(fontSize: 50,color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _restartButton() {
+    return Row(
+      children: [
+
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(Colors.blueGrey)
+                ),
+                onPressed: () {
+                  setState(() {
+                    initializeGame();
+                  });
+                },
+                child: const Text("BeginOver",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 35),)),
+          ),
+        ),
+        
+      ],
+      
+    );
+  }
+
+  changeTurn() {
+    if (currentPlayer == PLAYER_X) {
+      currentPlayer = PLAYER_Y;
+    } else {
+      currentPlayer = PLAYER_X;
+    }
+  }
+
+  checkForWinner() {
+    //Define winning positions
+    List<List<int>> winningList = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (var winningPos in winningList) {
+      String playerPosition0 = occupied[winningPos[0]];
+      String playerPosition1 = occupied[winningPos[1]];
+      String playerPosition2 = occupied[winningPos[2]];
+
+      if (playerPosition0.isNotEmpty) {
+        if (playerPosition0 == playerPosition1 &&
+            playerPosition0 == playerPosition2) {
+          //all equal means player won
+          showGameOverMessage("Player $playerPosition0 Won");
+          gameEnd = true;
+          return;
+        }
+      }
+    }
+  }
+
+  checkForDraw() {
+    if (gameEnd) {
+      return;
+    }
+    bool draw = true;
+    for (var occupiedPlayer in occupied) {
+      if (occupiedPlayer.isEmpty) {
+        //at least one is empty not all are filled
+        draw = false;
+      }
+    }
+
+    if (draw) {
+      showGameOverMessage("Draw");
+      gameEnd = true;
+    }
+  }
+
+  showGameOverMessage(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+          backgroundColor: Colors.blueGrey,
+          content: Text(
+            "Game Over \n $message",
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 18, fontFamily: 'Almendra',
+            ),
+          )),
     );
   }
 }
--- TicTacToeGames/main.dart
+++ TicTacToeGames/main.dart
@@ -6,9 +6,6 @@ void main(){
   return runApp(tic_tac_toe());
 }
 
-String player1 = '', player2 = '', symbol1 = '', symbol2 = '', symbol3 = '', symbol4 = '', symbol5 = '', symbol6 = '', symbol7 = '', symbol8 = '', symbol9 = '';
-int flag1 = 1, flag2 = 2, count = 0;
-
 class tic_tac_toe extends StatefulWidget {
   const tic_tac_toe({super.key});
 
@@ -21,8 +18,9 @@ class _tic_tac_toeState extends State<tic_tac_toe> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Almendra'),
 
-      home: game(),
+      home: GamePage(),
 
     );
   }
